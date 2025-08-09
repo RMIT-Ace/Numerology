e3ebce11ce897b12e47837a1172e59a18d5d20fc
RMIT-Ace <ace.authors@rmit.edu.au>
2025-08-09 21:51:37 +1000

    Added sample Simple Numero view

---
+++ Sources/Numerology/View/SimpleNumerologyView.swift
@@ -0,0 +1,65 @@
+//
+//  NumerologyView.swift
+//  Numerology
+//
+//  Created by Ace on 9/8/2025.
+//
+
+
+import SwiftUI
+
+public struct SimpleNumerologyView: View {
+    @Environment(NumerologyViewModel.self) var vm
+    
+    public var body: some View {
+        @Bindable var vm = vm
+        List {
+            HScrollSectionView($vm.digits) { digit in
+                UpDownNumberPickerView(digit: digit)
+            } header: {
+                Text("Inputs (\(vm.digits.count)):")
+            } footer: {
+                HStack {
+                    Spacer()
+                    RoundedCornerButton("C") { vm.resetAllDigits() }
+                    RoundedCornerButton("+") { vm.maxDigits += 1 }
+                    RoundedCornerButton("-") { vm.maxDigits -= 1 }
+                }
+            }
+            
+            Section {
+                HStack {
+                    Spacer()
+                    Text("\(vm.numerologyNumber)")
+                        .font(.system(size: 48))
+                        .bold()
+                    Spacer()
+                }
+            } header: {
+                Text("Numerology number:")
+            } footer: {
+                HStack {
+                    Spacer()
+                    Toggle("Reduce", isOn: $vm.isMasterNumberReduced)
+                        .toggleStyle(SwitchToggleStyle())
+                        .frame(width: 130)
+                }
+            }
+            
+            Section("Prediction") {
+                Text(vm.prediction)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .italic()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+
+        }
+    }
+    
+}
+
+#Preview {
+    var vm = NumerologyViewModel()
+    SimpleNumerologyView()
+        .environment(vm)
+}
